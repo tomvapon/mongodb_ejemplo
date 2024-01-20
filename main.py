30f016eb740949097d9c83df90f7a539f972ef64
tomvapon <tomasvarona91@gmail.com>
2024-01-20 22:05:44 +0100

    Type info

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ if __name__ == "__main__":
     mongo_operator = MongoOperator("local")
 
     # Create a collection named "pokemon"
-    mongo_operator.create_collection("pokemon")
+    mongo_operator.create_collection("pokemon_types")
 
     # Insert pokemon list in our pokemon collection
-    pokemon_json = 'data/pokemon.json'
+    pokemon_json = 'data/pokemon_types.json'
     with open(pokemon_json, 'r') as file:
         pokemon_data = file.read()
     
     pokemon_dict = json.loads(pokemon_data)
 
     mongo_operator.insert_documents(
-        "pokemon", 
-        pokemon_dict['pokemon']
+        "pokemon_types", 
+        pokemon_dict['pokemon_types']
     )
